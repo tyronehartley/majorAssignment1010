406b79f3af9e05847deeb6b7ff49b441704dee65
tyronehartley <tyronehartley@gmail.com>
2024-10-12 15:13:56 +1100

    updated work

--- majorAssignmentTopic2/src/Library.java
+++ majorAssignmentTopic2/src/Library.java
@@ -25,5 +25,42 @@ public class Library {
             song.printSongDetails();
         }
     }
-     // Additional methods can be added here (remove song, shuffle)
+    //method to remove song from library
+    public void removeSong(Song song){
+        songs.remove(song);
+    }
+
+    //method to get all songs by an artist
+    public void getSongByArtist(Artist target) {
+        //library null check
+        if (songs.isEmpty()) {
+            System.out.println("Sorry! No Songs in the Library");
+            return;
+        }
+        //enhanced loop checking if artist attribute of each song matches target variable, prints
+        System.out.println("Songs by this Artist:");
+        for (Song song : songs) {
+            if(song.artist == target){
+            song.printSongDetails();
+            }
+        }
+    }
+
+    public void getSongByAlbum(Album target) {
+        //library null check
+        if (songs.isEmpty()) {
+            System.out.println("Sorry! No Songs in the Library");
+            return;
+        }
+
+        System.out.println("Songs in this album:");
+        //album attribute of song needs to be changed from a String to an Album
+        for (Song song : songs) {
+            if(song.album == target){
+            song.printSongDetails();
+            }
+        }
+        return;
+
+    }
 }
